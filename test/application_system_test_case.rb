7f398dfbff218b7353312c4021a3b7ee64a8c6ed
Michael Jelks <spam@zaftig.net>
2025-02-11 18:16:54 -0800

    Update ci.yml Add vendor/cache for bundle gems remove bundler cache no cache Lint it up add overmind and fix func tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -11,4 +11,15 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   else
     driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
   end
+
+  def login_as(user)
+    # Assuming you have a login path and a form with `email` and `password`
+    visit new_session_url
+    fill_in "email_address", with: user.email_address
+    fill_in "password", with: "password"
+    within("form") do
+      # click_on "Sign in"
+      click_button "commit"
+    end
+  end
 end
--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,37 +1,46 @@
 require "application_system_test_case"
+require "test_helper"
 
 class UsersTest < ApplicationSystemTestCase
+  fixtures :users
+
   setup do
-    @user = users(:one)
+    @user = users(:default_user)
   end
 
   test "visiting the index" do
     visit users_url
+    # default behavior is to redirect to the page after logging in
+    login_as(@user)
     assert_selector "h1", text: "Users"
   end
 
   test "should create user" do
     visit users_url
+    login_as(@user)
+
     click_on "New user"
+    fill_in "Email address", with: "foo@example.com"
+    find("input[name='user[password_digest]']").set("mysecretpassword")
 
     click_on "Create User"
-
     assert_text "User was successfully created"
-    click_on "Back"
   end
 
   test "should update User" do
     visit user_url(@user)
-    click_on "Edit this user", match: :first
+    login_as(@user)
 
+    click_on "Edit this user", match: :first
+    find("input[name='user[password_digest]']").set("mysecretpassword-deux")
     click_on "Update User"
 
     assert_text "User was successfully updated"
-    click_on "Back"
   end
 
   test "should destroy User" do
     visit user_url(@user)
+    login_as(@user)
     click_on "Destroy this user", match: :first
 
     assert_text "User was successfully destroyed"
