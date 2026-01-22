486b16198e2756324239d83ddf527362b55b53fa
mjelks <spam@zaftig.net>
2026-01-22 23:44:07 +0000

    update latest code

--- test/models/media_item_test.rb
+++ test/models/media_item_test.rb
@@ -2,24 +2,23 @@
 #
 # Table name: media_items
 #
-#  id            :integer          not null, primary key
-#  play_count    :integer
-#  title         :string
-#  track_count   :integer
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  artist_id     :integer          not null
-#  media_type_id :integer          not null
+#  id             :integer          not null, primary key
+#  name           :string
+#  play_count     :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  media_owner_id :integer          not null
+#  media_type_id  :integer          not null
 #
 # Indexes
 #
-#  index_media_items_on_artist_id      (artist_id)
-#  index_media_items_on_media_type_id  (media_type_id)
+#  index_media_items_on_media_owner_id  (media_owner_id)
+#  index_media_items_on_media_type_id   (media_type_id)
 #
 # Foreign Keys
 #
-#  artist_id      (artist_id => artists.id)
-#  media_type_id  (media_type_id => media_types.id)
+#  media_owner_id  (media_owner_id => media_owners.id)
+#  media_type_id   (media_type_id => media_types.id)
 #
 require "test_helper"
 
