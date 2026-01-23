849bb116e341341cef63014494635378c2b80d04
mjelks <spam@zaftig.net>
2026-01-23 02:33:54 +0000

    update user tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -13,7 +13,12 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   end
 
   def login_as(user)
-    # Assuming you have a login path and a form with `email` and `password`
+    # Wait for the modal to be visible and form to be ready
+    assert_selector "#modal", visible: true
+    assert_selector "#modal input[name='email_address']", visible: true
+    assert_selector "#modal input[name='password']", visible: true
+    assert_button "Sign in", disabled: false
+
     within("#modal") do
       fill_in "email_address", with: user.email_address
       fill_in "password", with: "password"
