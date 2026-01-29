35318aa8ae20494faefe359b783f124e83286439
mjelks <spam@zaftig.net>
2026-01-29 19:13:38 +0000

    update test coverage locations

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,12 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 require "simplecov"
-SimpleCov.start
+SimpleCov.start "rails" do
+  add_filter "/test/"
+  add_filter "/lib/itunes_import/"
+
+  track_files "{app,lib}/**/*.rb"
+end
 
 module ActiveSupport
   class TestCase
